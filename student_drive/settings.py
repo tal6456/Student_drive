be806594a22ed43770053bf994272c3353f44f94
tal64 <tal6456@gmailcom>
2026-03-11 14:47:38 +0200

    kozo i add value

--- student_drive/settings.py
+++ student_drive/settings.py
@@ -157,4 +157,4 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 ACCOUNT_LOGOUT_ON_GET = True
 SOCIALACCOUNT_LOGIN_ON_GET = True
 #hey kozo
-#nfosnfn
+#nfosnfnסחעה'חVDJHVWHDJJKHDWJQDJHDJHEWJNDWEJHCD J
