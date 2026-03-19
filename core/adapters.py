ca135c50513d69cbbdde63195018e932e71fae9b
tal64 <tal6456@gmailcom>
2026-03-19 21:17:01 +0200

    19.3 21:16 עדכון של ההרשמה לאחר שיש לך כבר משתמש רשום

--- core/adapters.py
+++ core/adapters.py
@@ -2,12 +2,26 @@ from allauth.account.adapter import DefaultAccountAdapter
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
 from django.urls import reverse
 
+
 class CustomAccountAdapter(DefaultAccountAdapter):
     def get_signup_redirect_url(self, request):
-        # מופעל רק כשמשתמש חדש נרשם עם אימייל וסיסמה
+        # מופעל רק כשמשתמש חדש נרשם עם אימייל וסיסמה פעם ראשונה
         return reverse('complete_profile')
 
+    def get_login_redirect_url(self, request):
+        # מופעל בכל פעם שיש התחברות (בין אם רגילה ובין אם חיבור אוטומטי של גוגל למשתמש קיים)
+        user = request.user
+
+        # הלוגיקה החכמה: בודק אם הפרופיל חסר פרטים בסיסיים (למשל, שם פרטי)
+        # אם אין לו שם פרטי, זה אומר שהוא בחיים לא סיים את טופס "השלמת פרטים"
+        if not user.first_name:
+            return reverse('complete_profile')
+
+        # אם יש לו הכל, ברוך הבא לדף הבית!
+        return reverse('home')
+
+
 class CustomSocialAccountAdapter(DefaultSocialAccountAdapter):
-    def get_signup_redirect_url(self, request):
-        # מופעל רק כשמשתמש חדש נרשם דרך גוגל פעם ראשונה
+    def get_signup_redirect_url(self, request, sociallogin):
+        # מופעל רק כשמשתמש חדש נרשם דרך גוגל פעם ראשונה (שים לב שהוספנו את sociallogin)
         return reverse('complete_profile')
