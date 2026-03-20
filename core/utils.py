95efa0ab47355fa6e982acdbc7f37f99253bf1f3
tal64 <tal6456@gmailcom>
2026-03-20 21:05:05 +0200

    20.3 20:29 תיקון דחיסה 3

--- core/utils.py
+++ core/utils.py
@@ -2,6 +2,7 @@ import os
 from io import BytesIO
 from PIL import Image
 from django.core.files.base import ContentFile
+from django.core.exceptions import ValidationError
 
 
 def compress_to_webp(image_field, max_size=(1200, 1200), quality=80):
@@ -25,3 +26,14 @@ def compress_to_webp(image_field, max_size=(1200, 1200), quality=80):
     new_filename = f"{os.path.splitext(original_name)[0]}.webp"
 
     return ContentFile(output.read(), name=new_filename)
+
+# ==============================================
+# פונקציית הגבלת העלאת קבצים של עד 20 מגה בייט
+# ==============================================
+def validate_file_size(value):
+    """
+    מוודא שקובץ שהועלה לא חורג מהמשקל המקסימלי המותר (20MB)
+    """
+    limit_mb = 20
+    if value.size > limit_mb * 1024 * 1024:
+        raise ValidationError(f"אופס! הקובץ גדול מדי ({limit_mb}MB מקסימום). כדי לשמור על האתר מהיר לכולם, אנא כווץ את הקובץ (ניתן לעשות זאת בחינם באתר iLovePDF) ונסה שוב.")
