36ecfa307c88426bef4dbf20024d50db348e822c
tal64 <tal6456@gmailcom>
2026-03-21 12:52:45 +0200

    Link migration 4 directly to 1

--- core/migrations/0004_remove_message_conversation_remove_message_sender_and_more.py
+++ core/migrations/0004_remove_message_conversation_remove_message_sender_and_more.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ('core', '0002_conversation_message'),
+        ('core', '0001_initial'),
     ]
 
     operations = [
